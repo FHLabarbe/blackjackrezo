c5fec23f3c1be15bebd9d2426dc34258a98f8e03
Baptiste Le Gouhinec <baptiste.le-gouhinec@etu.univ-orleans.fr>
2022-11-24 00:49:38 +0100

    modif fonction partie, pas fini en plein

--- serverblackjack.py
+++ serverblackjack.py
@@ -108,50 +108,84 @@ def score(main):
         else:
             s += main[i][0]
     return s
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# Fonction qui demande au joueurs leurs choix
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+async def ask_joueurs(reader,writer): # ajouter en paramètre le reader/writer mais je sais pas trop comment gerer ca
+    writer.write(f"Que voulez-vous faire ? \"MORE 0\" pour passer ou \"MORE 1\" pour piocher.".encode() + b"\r\n") 
+    await writer.drain()
+
+    #Récupération du choix
+    data = await reader.readline()
+    localChoix = data[6].decode() # on prend le 6e caractère ("MORE 1" on prend le "1")
+    writer.write(f"Choix enregistré.".encode() + b"\r\n")
+    await writer.drain()
     
+    writer.close()
+
+    return localChoix
+
+def ilyatildesjoueursactifsdanslavion(joueurs):
+    res = False
+    for i in joueurs:
+        res = res or i
+    
+    return res
+
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Fonction qui gère tout
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-def partie_multi(n):
+async def partie_multi(joueurs):
 # On initialise une liste de liste dans lesquels on viendra mettre les cartes. Le +1 correspond au croupier qui sera toujours la première main de la liste    
-    mains = [[]] * (n+1)
-    choix = [] * n
-    score_joueurs = [[]] * n
+    n = len(joueurs) #nbr de joueur(s)
+    mains = [[]] * (n+1) # liste qui garde les mains des joueurs
+    joueurs_actifs = [True] * n # sert de "compteur de joueur(s) actif(s)"
+    choix = [] * n # pour sauvegarder les choix de chacun
+    scores = [[]] * (n+1) # pour sauvegarder les scores des joueurs
 
     deck = melanger_deck(creer_deck())
 
-    for i in range(0,len(mains)) :
+    # le serveur pioche une carte
+    mains[0].append(deck[-1])
+    deck.pop
+
+    # on distribue deux cartes par joueur
+    for i in range(1,len(mains)):
         for j in range(2):
             mains[i].append(deck[-1])
             deck.pop
 
-    compteur_voir = 0
-
-    while(compteur_voir == 0):
-        # on attend les choix
-        # on rempli la liste des choix en fonction des reponses des joueurs
-
+    # il faudrais afficher les cartes de chaque joueur sur leur ecran, on devrait pouvoir faire ca avec une fonction qui envoie masse de writer et en string les trucs que j'utilisais
+    #   dans la v1 du jeu OU SINON juste renvoyer genre "Vos cartes : 2 de ♥, R de ♣" etc (histoire de pas s'embeter avec les affichages vu qu'il y a 0 points dessus)
+            
+    while(ilyatildesjoueursactifsdanslavion(joueurs_actifs)): 
+      
+        
         for i in range(0,n):
-            if choix[i] == "v" :
-                compteur_voir +=1
-            elif choix[i] == "p" :
-                mains[i+1].append(deck[-1])
-                deck.pop
+            # on calcule le score du joueur pour savoir si il peut jouer ou pas
+            if score(mains[i+1])<=21:
+                
+                choixJoueur = ask_joueurs() # ajouter en paramètre le reader/writer mais je sais pas trop comment faire pour les recup etc // # on demande aux joueurs quel est leur choix
 
-        if compteur_voir<n:
-            compteur_voir = 0
-    
-    score_serveur = score(mains[0])
+                if choixJoueur == "1": # si c'est 1 on pioche
+                    mains[i+1].append(deck[-1])
+                    deck.pop           # du coup ici aussi il faudrait lui afficher la nouvelle "carte" et son score.
+                else :
+                    choix[i] = 0
+            else :
+                choix[i] = 0
 
-    while(score_serveur<16) :
+    while(scores[0]<16) : # le croupier fini de jouer si besoin (- de 16)
         mains[i+1].append(deck[-1])
         deck.pop
-
-        score_serveur = mains[0]
     
     for i in range(0,n):
-        score_joueurs[i] = score(mains[i+1])
+        scores[i] = score(mains[i+1]) # on calcule les scores de tout le monde ( on devrait pouvoir le faire plus tôt je pense mais c'est plus simple ici pour l'instant)
+
+    # et la il faut faire en sorte de renvoyer les scores a chacun + le score du croupier
     
 
 if __name__ == '__main__':

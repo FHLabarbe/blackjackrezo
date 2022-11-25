5d87ed4c3078be851df80757394fad827e8048dd
François-Hugues Labarbe <francois-hugues.labarbe@etu.univ-orleans.fr>
2022-11-25 01:39:12 +0100

    rectification modif repo

--- serverblackjack.py
+++ serverblackjack.py
@@ -5,7 +5,6 @@ from sys import argv
 from socket import gethostbyname
 import asyncio
 import random
-from operator import countOf
 import os
 
 HOST = "10.0.1.1"
@@ -14,12 +13,8 @@ PORT_PLAYER = "667"
 tableName = str
 delay = int
 playerName = str
-playerWriter = asyncio.StreamWriter
-playerReader = asyncio.StreamReader
 players = {playerName : tableName}
-players = { playerName : [playerReader,playerWriter,tableName]} # le joueur est un nom, un reader, un writer et une table
 tables = {tableName : delay}
-waitingTables = []
 
 async def handle_dealer_request(reader, writer):
 
@@ -39,30 +34,30 @@ async def handle_dealer_request(reader, writer):
     await writer.drain()
 
     #insertion de la table dans le dictionnaire. La clé est le nom de la table, et le délai associé : la valeur.
-    tables[localTableName] = int(localDelay)
-    waitingTables.append(localTableName) # ajoute le nom de la table aux waitingTable. Le décompte se lancera quand le premier joueur rejoindra
+    tables[localTableName] = localDelay
+    
     writer.close()
 
 
 async def handle_player_request(reader,writer):
 
-    writer.write(f"Bienvenue sur le serveur de blackjack.".encode() + b"\r\n") 
-    await writer.drain()
+    data = await reader.read() #préciser le nb n de bytes à lire, sinon écrire readline à la place de read 
 
-    tableName = await reader.readline() ## Il faut 2 cas, celui où le joueur est le premier arrivé, et celui il y a déjà du monde
-    for table in waitingTables:
-        if tableName == table and (countOf(players.values(), tableName) < 1): # on vérifie que la table existe et compte le nb de fois qu'un joueur a cette table d'associée
-            playerName = writer.get_extra_info('peername'[0])
-            players[playerName] = [reader,writer,tableName] # ajoute le joueur ainsi que ses readers et writers dans le dictionnaire
-            waitDelay(tableName)
-        elif tableName == table and (countOf(players.values(), tableName) > 1): # gérer l'asynchronicité pour que le joueur ne rejoigne pas si la table est fermée
-            playerName = writer.get_extra_info('peername'[0])
-            players[playerName] = [reader,writer,tableName] # ajoute le joueur ainsi que ses readers et writers dans le dictionnaire
-        else :
-            writer.write("END".encode())
-            writer.close()
-            return
-    writer.write(f'recu, vous entrez dans la table {tableName}\n') # ajouter le cas ou le nom de table n'est pas bon
+    if data == "NAME": # nom d'une table
+        writer.write(f'entrer le nom :\n')
+        tableName = await reader.read()
+        writer.write(f'recu, vous entrer dans la table {tableName}\n') # ajouter le cas ou le nom de table n'est pas bon
+        playerName = writer.get_extra_info('peername'[0])
+        players[playerName] = tableName
+
+    if data == "p": # demande de pioche
+        writer.write(f'Votre nouvelle carte est :\n')
+
+    if data == "p": # demande de voir
+        writer.write(f'Vous avez fini de jouer, votre score final est de : ... , en attente des autres joueurs\n')
+
+    writer.close()
+    reader.close()
 
 async def blackjack_server():
     # démarre le serveur
@@ -74,10 +69,6 @@ async def blackjack_server():
         await server_player.serve_forever() 
 
 
-async def waitDelay(tableName):
-    asyncio.sleep(tables[tableName])
-    del waitingTables[tableName]
-    # lancer la partie.
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Initialisation du jeu de carte
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -122,9 +113,10 @@ def score(main):
 # Fonction qui demande au joueurs leurs choix
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-async def ask_joueurs(playerName):
-    reader = players[playerName].values([0])
-    writer = players[playerName].values([1])
+async def ask_joueurs(reader,writer): # ajouter en paramètre le reader/writer mais je sais pas trop comment gerer ca
+    writer.write(f"Que voulez-vous faire ? \"MORE 0\" pour passer ou \"MORE 1\" pour piocher.".encode() + b"\r\n") 
+    await writer.drain()
+
     #Récupération du choix
     data = await reader.readline()
     localChoix = data[6].decode() # on prend le 6e caractère ("MORE 1" on prend le "1")
@@ -146,13 +138,13 @@ def ilyatildesjoueursactifsdanslavion(joueurs):
 # Fonction qui gère tout
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-async def partie_multi(joueurs,table):
+async def partie_multi(joueurs):
 # On initialise une liste de liste dans lesquels on viendra mettre les cartes. Le +1 correspond au croupier qui sera toujours la première main de la liste    
-    n = len(joueurs.keys()) #nbr de joueur(s)
+    n = len(joueurs) #nbr de joueur(s)
     mains = [[]] * (n+1) # liste qui garde les mains des joueurs
     joueurs_actifs = [True] * n # sert de "compteur de joueur(s) actif(s)"
     choix = [] * n # pour sauvegarder les choix de chacun
-    scores = [0] * (n+1) # pour sauvegarder les scores des joueurs
+    scores = [[]] * (n+1) # pour sauvegarder les scores des joueurs
 
     deck = melanger_deck(creer_deck())
 
@@ -169,7 +161,7 @@ async def partie_multi(joueurs,table):
     # il faudrais afficher les cartes de chaque joueur sur leur ecran, on devrait pouvoir faire ca avec une fonction qui envoie masse de writer et en string les trucs que j'utilisais
     #   dans la v1 du jeu OU SINON juste renvoyer genre "Vos cartes : 2 de ♥, R de ♣" etc (histoire de pas s'embeter avec les affichages vu qu'il y a 0 points dessus)
             
-    while(ilyatildesjoueursactifsdanslavion(joueurs_actifs)): # rajouter que si un joueur est un 0, on l'appelle plus 
+    while(ilyatildesjoueursactifsdanslavion(joueurs_actifs)): 
       
         
         for i in range(0,n):
@@ -186,15 +178,12 @@ async def partie_multi(joueurs,table):
             else :
                 choix[i] = 0
 
-    scores[0] = scores(mains[0])
-    while(scores[0]<17) : # le croupier fini de jouer si besoin (- de 17)
+    while(scores[0]<16) : # le croupier fini de jouer si besoin (- de 16)
         mains[i+1].append(deck[-1])
         deck.pop
-        score
-        scores[0] = scores(mains[0])
-
-    for i in range(1,n):
-        scores[i] = score(mains[i]) # on calcule les scores de tout le monde ( on devrait pouvoir le faire plus tôt je pense mais c'est plus simple ici pour l'instant)
+    
+    for i in range(0,n):
+        scores[i] = score(mains[i+1]) # on calcule les scores de tout le monde ( on devrait pouvoir le faire plus tôt je pense mais c'est plus simple ici pour l'instant)
 
     # et la il faut faire en sorte de renvoyer les scores a chacun + le score du croupier
     

764e498fca78d7d64e0956b17825668cc058c6c5
Baptiste Le Gouhinec <baptiste.le-gouhinec@etu.univ-orleans.fr>
2022-11-26 02:42:53 +0100

    il doit manquer des choses mais il est tard et je veux dodo, normalement il y a pas d'erreur de syntaxe qui empeche de lancer le programme

--- serverblackjack.py
+++ serverblackjack.py
@@ -77,7 +77,8 @@ async def blackjack_server():
 async def waitDelay(tableName):
     asyncio.sleep(tables[tableName])
     del waitingTables[tableName]
-    # lancer la partie.
+    # lancer la partie. appeller la fonction partie
+
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Initialisation du jeu de carte
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -122,9 +123,8 @@ def score(main):
 # Fonction qui demande au joueurs leurs choix
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-async def ask_joueurs(playerName):
-    reader = players[playerName].values([0])
-    writer = players[playerName].values([1])
+async def ask_joueurs(reader, writer):
+    writer.write(f'.') # je crois que c'est ce qu'il faut envoyer pour activer la sequence de choix de carte (joueur.py ligne 33)
     #Récupération du choix
     data = await reader.readline()
     localChoix = data[6].decode() # on prend le 6e caractère ("MORE 1" on prend le "1")
@@ -142,23 +142,49 @@ def ilyatildesjoueursactifsdanslavion(joueurs):
     
     return res
 
+def affiche_mains(main,writer,score): # cette fonction affiche les cartes et le score de la main donnée en paramètre au joueur dont le writer est en paramètre
+    writer.write((f"Vos cartes sont :".encode() + b"\r\n"))
+    for i in main:
+        writer.write((f"{i}".encode() + b"\r\n"))
+    writer.write((f"Votre score est de : {score}".encode() + b"\r\n"))
+    writer.close()
+
+def quiGagne(scoreServeur,scoreJoueur,writer): #fonction qui compare les scores et qui annonce les résultat d'une partie + met fin a la communication (rajouter le fait de pas contacter ceux qui ont deja perdu)
+    if scoreServeur == scoreJoueur:
+        writer.write((f"égalité".encode() + b"\r\n"))
+    elif scoreServeur < scoreJoueur:
+        writer.write((f"vous gagner".encode() + b"\r\n"))
+    else:
+        writer.write((f"vous perdez".encode() + b"\r\n"))
+    writer.write((f"END".encode() + b"\r\n"))
+
+
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Fonction qui gère tout
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-async def partie_multi(joueurs,table):
-# On initialise une liste de liste dans lesquels on viendra mettre les cartes. Le +1 correspond au croupier qui sera toujours la première main de la liste    
-    n = len(joueurs.keys()) #nbr de joueur(s)
+async def partie_multi(joueurs,nomTable):
+    n = 0 
+    readerPartie = []
+    writerPartie = []
+
+    for i in joueurs.keys():
+        if joueurs[i][2] == nomTable:
+            readerPartie.append(joueurs[i][0]) #on stocke les reader
+            writerPartie.append(joueurs[i][1]) #et les writer (pour que ce soit plus facile a utiliser plus tard)
+            n+=1 #on compte aussi le nombre de joueurs
+
     mains = [[]] * (n+1) # liste qui garde les mains des joueurs
     joueurs_actifs = [True] * n # sert de "compteur de joueur(s) actif(s)"
     choix = [] * n # pour sauvegarder les choix de chacun
     scores = [0] * (n+1) # pour sauvegarder les scores des joueurs
 
-    deck = melanger_deck(creer_deck())
+    deck = melanger_deck(creer_deck()) # on creer le deck et on le melange
 
     # le serveur pioche une carte
     mains[0].append(deck[-1])
     deck.pop
+    scores[0] = score(mains[0])
 
     # on distribue deux cartes par joueur
     for i in range(1,len(mains)):
@@ -166,40 +192,51 @@ async def partie_multi(joueurs,table):
             mains[i].append(deck[-1])
             deck.pop
 
-    # il faudrais afficher les cartes de chaque joueur sur leur ecran, on devrait pouvoir faire ca avec une fonction qui envoie masse de writer et en string les trucs que j'utilisais
-    #   dans la v1 du jeu OU SINON juste renvoyer genre "Vos cartes : 2 de ♥, R de ♣" etc (histoire de pas s'embeter avec les affichages vu qu'il y a 0 points dessus)
+    # on calcule les scores
+    for i in range(1,n): 
+        scores[i] = score(mains[i])
+    
+    # on va envoyer a chaque joueur la carte du croupier + on affiche la main du joueur en question
+    for i in range(0,n):
+        writerPartie[i].write((f"La carte du serveur est : {mains[0][0]}".encode() + b"\r\n"))
+        affiche_mains(mains[i+1],writerPartie[i],score[i+1])
+
             
-    while(ilyatildesjoueursactifsdanslavion(joueurs_actifs)): # rajouter que si un joueur est un 0, on l'appelle plus 
+    while(ilyatildesjoueursactifsdanslavion(joueurs_actifs)):
       
-        
         for i in range(0,n):
             # on calcule le score du joueur pour savoir si il peut jouer ou pas
-            if score(mains[i+1])<=21:
-                
-                choixJoueur = ask_joueurs() # ajouter en paramètre le reader/writer mais je sais pas trop comment faire pour les recup etc // # on demande aux joueurs quel est leur choix
+            affiche_mains(mains[i+1])
+            if joueurs_actifs[i]:
+
+                choixJoueur = ask_joueurs(readerPartie[i],writerPartie[i])
 
                 if choixJoueur == "1": # si c'est 1 on pioche
                     mains[i+1].append(deck[-1])
-                    deck.pop           # du coup ici aussi il faudrait lui afficher la nouvelle "carte" et son score.
-                else :
-                    choix[i] = 0
-            else :
-                choix[i] = 0
+                    deck.pop
+                    scores[i+1]+=score(mains[i+1])
+                    writerPartie.write(f'Vous piochez une carte.'.encode() + b"\r\n")
+
+                    if score[i+1]>21: #on regarde tout de suite apres avoir pioché si le score est > 21 comme ca on peux deja virer le joueur
+                        joueurs_actifs[i] = False
+                        writerPartie.write(f'Vous avez dépasser 21, vous avez perdu.'.encode() + b"\r\n")
+                        affiche_mains(mains[i+1],writerPartie[i],score[i+1])
+                        writerPartie.write(f"END".encode())
+                        joueurs_actifs[i] = False
+                else:
+                    joueurs_actifs[i] = False
 
     scores[0] = scores(mains[0])
+
     while(scores[0]<17) : # le croupier fini de jouer si besoin (- de 17)
         mains[i+1].append(deck[-1])
         deck.pop
         score
         scores[0] = scores(mains[0])
 
-    for i in range(1,n):
-        scores[i] = score(mains[i]) # on calcule les scores de tout le monde ( on devrait pouvoir le faire plus tôt je pense mais c'est plus simple ici pour l'instant)
-
-    # et la il faut faire en sorte de renvoyer les scores a chacun + le score du croupier
+    for i in range(0,n):
+        quiGagne(scores[0],scores[i+1],writerPartie[i])
     
 
 if __name__ == '__main__':
     asyncio.run(blackjack_server())
-    #on renvoie les scores des joueurs
-    #fin de la partie
